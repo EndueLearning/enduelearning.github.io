f58505b909947826e243eaf3c638891146e5aee7
EndueLearning <enduelearning@gmail.com>
2025-11-05 16:06:15 +0530

    Update favourites.js

--- assets/js/favourites.js
+++ assets/js/favourites.js
@@ -1,1 +1,11 @@
-// favourites stub
+// assets/js/favourites.js
+function toggleFavourite(id){
+  const fav = JSON.parse(localStorage.getItem('endue_favs') || '[]');
+  const idx = fav.indexOf(id);
+  if(idx === -1) fav.push(id); else fav.splice(idx,1);
+  localStorage.setItem('endue_favs', JSON.stringify(fav));
+}
+function isFavourite(id){
+  const fav = JSON.parse(localStorage.getItem('endue_favs') || '[]');
+  return fav.includes(id);
+}
